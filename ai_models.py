874d01adcd68585de93a76edfb5f4f638737c1a9
Alkiviadis Tzaras <alkiviadis.tzaras@efta.int>
2025-03-04 16:36:27 +0100

    refactor: update AI models and dependencies in notebooks

--- ai_models.py
+++ ai_models.py
@@ -13,8 +13,5 @@ azure_embeddings_model = AzureOpenAIEmbeddings(
     api_key=settings.AZURE_OPENAI_EMBEDDING_API_KEY,
     model="text-embedding-3-small",
     openai_api_version="2023-05-15",
-    max_retries=3,
-    retry_min_seconds=4,  # Min time between retries
-    retry_max_seconds=20,  # Max time between retries
-    show_progress_bar=True,  # Shows progress of embedding
+
 )
